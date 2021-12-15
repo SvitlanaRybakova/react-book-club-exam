01c4b63d932d5830e9e708f83478ef7cfd992235
Svitlana <devop.edu@gmail.com>
2021-12-15 16:59:24 +0100

    changed state if URL params have been changed

--- src/contexts/SearchContext.js
+++ src/contexts/SearchContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useLocation } from "react-router-dom";
 
 export const SearchContext = createContext();
 
@@ -11,6 +11,8 @@ const SearchContextProvider = ({ children }) => {
   let [searchParams, setSearchParams] = useSearchParams({});
   const searchTerm = searchParams.get("searchText") || "";
 
+  const location = useLocation();
+
   const [query, setQuery] = useState({
     searchText: "",
     lang: "en",
@@ -18,17 +20,24 @@ const SearchContextProvider = ({ children }) => {
     popular: false,
   });
 
-  // changes the browser URL every time when the query has been changed
+  // changes the browser URL every time when the query has been changed(date copy from state to URL)
   useEffect(() => {
     handleUrl();
   }, [query]);
 
+  // triggers state when URL changes by clicking browser back btn
+  // (date copy from URL to state)
+  useEffect(() => {
+    const obj = Object.fromEntries(new URLSearchParams(location.search));
+    setQuery({ ...obj });
+  }, [searchParams]);
+
   const handleUrl = () => {
     const params = {};
-    if (query.searchText.length) {
+    if (query.searchText?.length) {
       params.searchText = query.searchText;
     }
-    if (query.lang.length) {
+    if (query.lang?.length) {
       params.lang = query.lang;
     }
     if (query.genre) {
